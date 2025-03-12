bff20208400cd168012ea89f89be8fc8e927ddc0
vlkttlv <vitavitavita04@bk.ru>
2025-03-12 17:43:07 +0700

    commit

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, CustomersListView, OrdersListView
+from .views import BookListView, HomePageView, CustomersListView, OrdersListView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('customers', CustomersListView.as_view(), name='customers'),
     path('orders', OrdersListView.as_view(), name='orders'),
+    path('books', BookListView.as_view(), name='books'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
-from shop.models import Customer, Order
+from shop.models import Book, Customer, Order
 
 
 class HomePageView(TemplateView):
@@ -17,3 +17,9 @@ class OrdersListView(ListView):
     template_name = "orders.html"
     model = Order
     context_object_name = "orders"
+
+
+class BookListView(ListView):
+    template_name = "books.html"
+    model = Book
+    context_object_name = "books"
