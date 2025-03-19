77c9c9fee85af5be7fa3947952789fbed72f51e8
vlkttlv <vitavitavita04@bk.ru>
2025-03-19 22:54:19 +0700

    add books pages

--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import HomePageView, CustomersListView, OrdersListView, OrderDetailView
+from .views import (HomePageView,
+                    CustomersListView,
+                    OrdersListView,
+                    OrderDetailView,
+                    BookDetailView,
+                    BooksListView)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('customers', CustomersListView.as_view(), name='orders'),
     path('orders', OrdersListView.as_view(), name='orders'),
     path('orders/<int:pk>', OrderDetailView.as_view(), name='order_detail'),
+    path('books', BooksListView.as_view(), name='books'),
+    path('books/<int:pk>', BookDetailView.as_view(), name='book_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from shop.models import Customer, Order
+from shop.models import Customer, Order, Book
 
 
 class HomePageView(TemplateView):
@@ -24,3 +24,15 @@ class OrderDetailView(DetailView):
     template_name = "order_detail.html"
     model = Order
     context_object_name = "order"
+
+
+class BooksListView(ListView):
+    template_name = "books.html"
+    model = Book
+    context_object_name = "list_of_all_books"
+
+
+class BookDetailView(DetailView):
+    template_name = "book_detail.html"
+    model = Book
+    context_object_name = "book"
