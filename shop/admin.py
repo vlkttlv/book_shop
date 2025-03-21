5a48dec2d8f84827c707b9270bc706df1efc24a6
vlkttlv <vitavitavita04@bk.ru>
2025-03-21 13:40:59 +0700

    добавила модель

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from shop.models import Customer, CartItem, Comment, Book, Address, Order, Category
+from shop.models import Customer, CartItem, Comment, Book, Address, Order, OrderItem, Category
 
 
 admin.site.register(Customer)
 admin.site.register(Order)
+admin.site.register(OrderItem)
 admin.site.register(CartItem)
 admin.site.register(Comment)
 admin.site.register(Book)
