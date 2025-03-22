428fe24e56abf93f0917e7fb3adeaf2aae7ffef0
vlkttlv <vitavitavita04@bk.ru>
2025-03-22 16:37:43 +0700

    urls

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from .views import (HomePageView,
                     CustomersListView,
                     OrdersListView,
@@ -13,4 +15,4 @@ urlpatterns = [
     path('orders/<int:pk>', OrderDetailView.as_view(), name='order_detail'),
     path('books', BooksListView.as_view(), name='books'),
     path('books/<int:pk>', BookDetailView.as_view(), name='book_detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
